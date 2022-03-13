a34ccdcb41d2ad768b8ae48da692e90baca7b5f3
raybloo <raybloo@ymail.com>
2022-03-13 13:19:28 +0100

    1st prototype

--- Assets/Scripts/GameSuperviser.cs
+++ Assets/Scripts/GameSuperviser.cs
@@ -7,9 +7,12 @@ public class GameSuperviser : MonoBehaviour
 {
 
     public GameObject[] plants;
-    public GameObject[] tools;
+    public GameObject[] enemies;
+    public PickupItem[] tools;
     public GameObject herbarium;
     public Text timerDisplay;
+    public Text levelDisplay;
+    public GameObject gameoverDisplay;
     public GameObject arrosoirUI;
     public GameObject sprayUI;
     public GameObject shovelUI;
@@ -22,11 +25,15 @@ public class GameSuperviser : MonoBehaviour
     public GameObject ricinusUI;
     public GameObject veratrumUI;
 
+    public List<GameObject> plantsPlanted;
+    public List<GameObject> enemiesPlanted;
+
     public float timer = 0f;
 
     public Character[] characters;
 
-    public (int, 
+    public (float,
+        int, 
         int, 
         PickupItem.ItemType,
         int,
@@ -46,11 +53,14 @@ public class GameSuperviser : MonoBehaviour
     public bool gameover = false;
     private bool timerRunning = false;
     private bool gamePaused = false;
+    private bool allPlantsTended = false;
+    private GameObject lastCreated;
     // Start is called before the first frame update
     void Start()
     {
         currentLevel = 1;
-        config = new (int,
+        config = new (float,
+        int,
         int,
         PickupItem.ItemType,
         int,
@@ -64,10 +74,15 @@ public class GameSuperviser : MonoBehaviour
         int,
         PickupItem.ItemType,
         int,
-        PickupItem.ItemType)[3];
-        config[0] = (0, 0, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel);
-        config[1] = (0, 1, PickupItem.ItemType.Arrosoir, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel);
-        config[2] = (0, 3, PickupItem.ItemType.Spray, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel);
+        PickupItem.ItemType)[5];
+        config[0] = (60f, 0, 3, PickupItem.ItemType.Arrosoir, 4, PickupItem.ItemType.Spray, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel);
+        config[1] = (60f, 0, 3, PickupItem.ItemType.Arrosoir, 4, PickupItem.ItemType.Spray, 3, PickupItem.ItemType.Spray, 2, PickupItem.ItemType.Arrosoir, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel);
+        config[2] = (45f, 0, 1, PickupItem.ItemType.Spray, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Arrosoir, 1, PickupItem.ItemType.Spray, 1, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel, 0, PickupItem.ItemType.Shovel);
+        config[3] = (45f, 0, 3, PickupItem.ItemType.Spray, 2, PickupItem.ItemType.Shovel, 3, PickupItem.ItemType.Shovel, 4, PickupItem.ItemType.Shovel,3, PickupItem.ItemType.Arrosoir, 1, PickupItem.ItemType.Shovel, 1, PickupItem.ItemType.Shovel);
+        config[4] = (30f, 0, 3, PickupItem.ItemType.Arrosoir, 2, PickupItem.ItemType.Shovel, 2, PickupItem.ItemType.Arrosoir, 1, PickupItem.ItemType.Shovel, 3, PickupItem.ItemType.Arrosoir, 2, PickupItem.ItemType.Spray, 4, PickupItem.ItemType.Spray);
+
+
+
 
         StartCoroutine(Game());
     }
@@ -76,6 +91,11 @@ public class GameSuperviser : MonoBehaviour
     void Update()
     {
         if(timerRunning) {
+            bool accTended = true;
+            foreach(GameObject plant in plantsPlanted) {
+                accTended = accTended && plant.GetComponent<Plant>().tended;
+            }
+            allPlantsTended = accTended;
             if (timer > 0f) {
                 timerDisplay.text = timer.ToString("00") + ":" + ((timer * 100f) % 100f).ToString("00");
                 timer -= Time.unscaledDeltaTime;
@@ -99,11 +119,14 @@ public class GameSuperviser : MonoBehaviour
         currentLevel = 1;
         while(!gameover) {
 
-            //Setup Herbarium
+            //Setup Variables
+            timer = 0f;
+            float levelTimer;
             int catNum;
             int belladonnaNum, cannabisNum, cathaNum, colchicumNum, leucanthemumNum, ricinusNum, veratrumNum;
             PickupItem.ItemType belladonnaTool, cannabisTool, cathaTool, colchicumTool, leucanthemumTool, ricinusTool, veratrumTool;
-            (catNum,
+            (levelTimer,
+                catNum,
                 belladonnaNum,
                 belladonnaTool,
                 cannabisNum,
@@ -119,9 +142,11 @@ public class GameSuperviser : MonoBehaviour
                 veratrumNum, 
                 veratrumTool) = config[currentLevel - 1];
 
+            //Display level
+            levelDisplay.text = "Level " + currentLevel;
 
             //Setup Herbarium
-            if(belladonnaNum > 0) {
+            if (belladonnaNum > 0) {
                 belladonnaUI.SetActive(true);
                 if (belladonnaTool == PickupItem.ItemType.Arrosoir) {
                     belladonnaUI.transform.position = new Vector3(arrosoirUI.transform.position.x, belladonnaUI.transform.position.y, belladonnaUI.transform.position.z);
@@ -134,27 +159,178 @@ public class GameSuperviser : MonoBehaviour
                 belladonnaUI.SetActive(false);
             }
 
-            //First Instantiate Flowers
+            if (cannabisNum > 0) {
+                cannabisUI.SetActive(true);
+                if (cannabisTool == PickupItem.ItemType.Arrosoir) {
+                    cannabisUI.transform.position = new Vector3(arrosoirUI.transform.position.x, cannabisUI.transform.position.y, cannabisUI.transform.position.z);
+                } else if (cannabisTool == PickupItem.ItemType.Shovel) {
+                    cannabisUI.transform.position = new Vector3(shovelUI.transform.position.x, cannabisUI.transform.position.y, cannabisUI.transform.position.z);
+                } else {
+                    cannabisUI.transform.position = new Vector3(sprayUI.transform.position.x, cannabisUI.transform.position.y, cannabisUI.transform.position.z);
+                }
+            } else {
+                cannabisUI.SetActive(false);
+            }
+
+            if (cathaNum > 0) {
+                cathaUI.SetActive(true);
+                if (cathaTool == PickupItem.ItemType.Arrosoir) {
+                    cathaUI.transform.position = new Vector3(arrosoirUI.transform.position.x, cathaUI.transform.position.y, cathaUI.transform.position.z);
+                } else if (cathaTool == PickupItem.ItemType.Shovel) {
+                    cathaUI.transform.position = new Vector3(shovelUI.transform.position.x, cathaUI.transform.position.y, cathaUI.transform.position.z);
+                } else {
+                    cathaUI.transform.position = new Vector3(sprayUI.transform.position.x, cathaUI.transform.position.y, cathaUI.transform.position.z);
+                }
+            } else {
+                cathaUI.SetActive(false);
+            }
+
+            if (colchicumNum > 0) {
+                colchicumUI.SetActive(true);
+                if (colchicumTool == PickupItem.ItemType.Arrosoir) {
+                    colchicumUI.transform.position = new Vector3(arrosoirUI.transform.position.x, colchicumUI.transform.position.y, colchicumUI.transform.position.z);
+                } else if (colchicumTool == PickupItem.ItemType.Shovel) {
+                    colchicumUI.transform.position = new Vector3(shovelUI.transform.position.x, colchicumUI.transform.position.y, colchicumUI.transform.position.z);
+                } else {
+                    colchicumUI.transform.position = new Vector3(sprayUI.transform.position.x, colchicumUI.transform.position.y, colchicumUI.transform.position.z);
+                }
+            } else {
+                colchicumUI.SetActive(false);
+            }
+
+            if (leucanthemumNum > 0) {
+                leucanthemumUI.SetActive(true);
+                if (leucanthemumTool == PickupItem.ItemType.Arrosoir) {
+                    leucanthemumUI.transform.position = new Vector3(arrosoirUI.transform.position.x, leucanthemumUI.transform.position.y, leucanthemumUI.transform.position.z);
+                } else if (leucanthemumTool == PickupItem.ItemType.Shovel) {
+                    leucanthemumUI.transform.position = new Vector3(shovelUI.transform.position.x, leucanthemumUI.transform.position.y, leucanthemumUI.transform.position.z);
+                } else {
+                    leucanthemumUI.transform.position = new Vector3(sprayUI.transform.position.x, leucanthemumUI.transform.position.y, leucanthemumUI.transform.position.z);
+                }
+            } else {
+                leucanthemumUI.SetActive(false);
+            }
+
+            if (ricinusNum > 0) {
+                ricinusUI.SetActive(true);
+                if (ricinusTool == PickupItem.ItemType.Arrosoir) {
+                    ricinusUI.transform.position = new Vector3(arrosoirUI.transform.position.x, ricinusUI.transform.position.y, ricinusUI.transform.position.z);
+                } else if (ricinusTool == PickupItem.ItemType.Shovel) {
+                    ricinusUI.transform.position = new Vector3(shovelUI.transform.position.x, ricinusUI.transform.position.y, ricinusUI.transform.position.z);
+                } else {
+                    ricinusUI.transform.position = new Vector3(sprayUI.transform.position.x, ricinusUI.transform.position.y, ricinusUI.transform.position.z);
+                }
+            } else {
+               ricinusUI.SetActive(false);
+            }
+
+            if (veratrumNum > 0) {
+                veratrumUI.SetActive(true);
+                if (veratrumTool == PickupItem.ItemType.Arrosoir) {
+                    veratrumUI.transform.position = new Vector3(arrosoirUI.transform.position.x, veratrumUI.transform.position.y, veratrumUI.transform.position.z);
+                } else if (veratrumTool == PickupItem.ItemType.Shovel) {
+                    veratrumUI.transform.position = new Vector3(shovelUI.transform.position.x, veratrumUI.transform.position.y, veratrumUI.transform.position.z);
+                } else {
+                    veratrumUI.transform.position = new Vector3(sprayUI.transform.position.x, veratrumUI.transform.position.y, veratrumUI.transform.position.z);
+                }
+            } else {
+                veratrumUI.SetActive(false);
+            }
+
             //Move tools not held around
-            //Setup Herborium
-            //Display Herborium/pause
+            foreach(PickupItem tool in tools) {
+                if(!tool.held) {
+                    tool.transform.position = FindPos(38f, 20f);
+                }
+            }
+
+            // Instantiate Plants
+            for (int i = 0; i < belladonnaNum; i++) {
+                lastCreated = Instantiate(plants[0], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = belladonnaTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < cannabisNum; i++) {
+                lastCreated = Instantiate(plants[1], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = cannabisTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < cathaNum; i++) {
+                lastCreated = Instantiate(plants[2], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = cathaTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < colchicumNum; i++) {
+                lastCreated = Instantiate(plants[3], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = colchicumTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < leucanthemumNum; i++) {
+                lastCreated = Instantiate(plants[4], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = leucanthemumTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < ricinusNum; i++) {
+                lastCreated = Instantiate(plants[5], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = ricinusTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            for (int i = 0; i < veratrumNum; i++) {
+                lastCreated = Instantiate(plants[6], FindPos(38f, 20f), Quaternion.identity);
+                lastCreated.GetComponent<Plant>().toolNeeded = veratrumTool;
+                plantsPlanted.Add(lastCreated);
+            }
+
+            //Instantiate Enemies
+            for (int i = 0; i < catNum; i++) {
+                enemiesPlanted.Add(Instantiate(enemies[0], FindPos(38f, 20f), Quaternion.identity));
+            }
+
+            //Pause ready to start
             PauseGame();
             yield return new WaitWhile(() => gamePaused);
-            timer = 5f;
+            timer = levelTimer;
             timerRunning = true;
-            yield return new WaitWhile(() => timerRunning);
+
+
+            //Wait for end of level
+            yield return new WaitUntil(() => !timerRunning || gameover || allPlantsTended);
             yield return new WaitForSecondsRealtime(0.2f);
+            
+            
             currentLevel++;
-            if (currentLevel > config.Length) {
+            if (!allPlantsTended) {
                 gameover = true;
+                gameoverDisplay.SetActive(true);
                 break;
             }
+            if (currentLevel > config.Length) {
+                gameover = true;
+                gameoverDisplay.SetActive(true);
+                break;
+            }
+
+            //Clear the scene
+            foreach (GameObject plant in plantsPlanted) {
+                Destroy(plant);
+            }
+            foreach (GameObject enemy in enemiesPlanted) {
+                Destroy(enemy);
+            }
+            plantsPlanted.Clear();
         }
-        yield return new WaitForSecondsRealtime(0.2f);
-
-        
 
 
+        PauseGame();
+        herbarium.SetActive(false);
+        yield return new WaitWhile(() => gamePaused);
+        Application.Quit();
     }
 
     private void PauseGame() {
@@ -168,4 +344,18 @@ public class GameSuperviser : MonoBehaviour
         gamePaused = false;
         Time.timeScale = 1f;
     }
+
+    public Vector3 FindPos(float boundsX, float boundsY) {
+        Vector2 probbedPos = Vector2.zero;
+        for (int i = 0; i < 8; i++) {
+            probbedPos = new Vector2((-boundsX / 2) + (Random.value * boundsX), (-boundsY / 2) + (Random.value * boundsY));
+            RaycastHit2D hit = Physics2D.CircleCast(probbedPos, 1.5f, Vector2.up, 0f);
+            if (!hit) {
+                return probbedPos;
+            }
+        }
+        return probbedPos;   
+    }
+
+
 }
